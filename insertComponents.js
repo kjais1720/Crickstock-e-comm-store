01d5d9b9fc7af64da84d09fe2a87e119174c3d27
Krituraaj Anand <kjais2000@gmail.com>
2022-02-12 17:45:50 +0530

    Feat: Created Landing page

---
+++ insertComponents.js
@@ -0,0 +1,102 @@
+const createHeader = pathToRoot =>{
+    const header = document.createElement('header');
+    header.innerHTML = `
+                <button class="tr-btn tr-btn-icon hamburger-btn hide">
+                <i class="fas fa-bars"></i>
+                </button>
+                <div class="tr-heading d-flex">
+                    <a href='${pathToRoot}' class='logo'>
+                        <img src='${pathToRoot}assets/crickstock-logo.jpeg' alt = "Crickstock logo">
+                    </a>
+                </div>
+                <div class="tr-input-wrapper d-flex gap-sm">
+                    <input type="text" class="tr-input-item" placeholder="Search">
+                    <button class="tr-btn tr-btn-icon">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+                <nav class="tr-nav d-flex gap-md">
+                    <button class="tr-btn tr-btn-link">Login</button>
+                    <div class="badge-wrapper">
+                        <a href="#" class="tr-btn tr-btn-icon">
+                            <i class="fas fa-heart"></i>
+                        </a>
+                        <div class="badge badge-red">5+</div>
+                    </div>
+                    <a href="#" class="tr-btn tr-btn-icon">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </nav>
+
+    `
+    header.className = 'tr-header d-flex gap-lg justify-c-center f-wrap'
+    return header;
+}
+
+const createHorCard = product =>{
+    const pdtCard = document.createElement('article');
+    pdtCard.innerHTML = `
+                        <button class="heart-icon tr-btn tr-btn-icon">
+                            <i style="font-weight:${product.wishlisted ? 400 : 900};" class="fas fa-heart"></i>
+                        </button>
+                        <div class="tr-card-banner">
+                            <img src="${product.imgSrc}"
+                                alt="${product.name}">
+                        </div>
+                        <div class="flex-col justify-c-start">
+                            <div class="tr-card-header">
+                                <h2 class="title">${product.name}</h2>                                     
+                                <h3 class="subtitle">${product.brand}</h3>
+                            </div>
+                            <div class="tr-ratings-badge txt-left ratings-sm tr-ratings-badge-green">
+                                <i class="fas fa-star"></i>
+                                <span>${product.ratings}</span>
+                            </div>
+                            <div class="d-flex gap-sm">
+                                <div class="pdt-price">
+                                    <span class="curr-price txt-semibold">
+                                        ${product.price}
+                                    </span>
+                                    <span class="prev-price txt-semibold">
+                                        ${product.prevPrice}
+                                    </span>
+                                </div>
+                                <div class="txt-semibold txt-success">50% off</div>
+                            </div>
+                            ${product.carted ? '<div class="d-flex gap-md align-i-center"><p>Quantity: </p><div class="d-flex"><div class="align-s-center fas fa-plus"></div><div class="pd-x-sm">2</div><div class="align-s-center fas fa-minus"></div></div></div>'
+                            :''}
+                            <div class="tr-card-footer-links d-flex f-wrap gap-sm">
+                                <button class="tr-btn tr-btn-secondary">
+                                    <i class="fas fa-cart-arrow-down"></i>
+                                    ${product.carted ? 'Remove from cart':'Add to cart'}
+                                </button>
+                                <button class="tr-btn tr-btn-primary">
+                                    <i class="fas fa-bags-shopping"></i>
+                                    ${product.carted ? 'Move to wishlist':'Buy now'}
+                                </button>
+                            </div>
+                        </div>
+    `
+    if(product.badge){
+        const badge = document.createElement('div');
+        badge.innerHTML = product.badge;
+        badge.className = 'tr-card-badge';
+        pdtCard.appendChild(badge)
+    }
+    pdtCard.className = 'pdt-card tr-card tr-card-hor d-flex';
+    return pdtCard;
+}
+
+const insertHeader = (pathToRoot) =>{
+    const newHeader = createHeader(pathToRoot);
+    document.body.prepend(newHeader);
+}
+
+const insertHorCards = (productType)=>{
+    const productsList = products[productType];
+    const horCardsContainer = document.getElementById('productCardsContainer');
+    productsList.forEach(item => {
+        const newCard = createHorCard(item);
+        horCardsContainer.appendChild(newCard);
+    })
+}
